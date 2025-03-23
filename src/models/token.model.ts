0ccc51bc4c37ebddc4ea038bc4c38fd7f8164e76
fedorchaklm <fedorchak.luba@gmail.com>
2025-03-23 23:45:05 +0200

    added ref:User

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -1,10 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
+import { User } from "./user.model";
+
 const tokenSchema = new Schema(
     {
         accessToken: { type: String, required: true },
         refreshToken: { type: String, required: true },
-        _userId: { type: Schema.Types.ObjectId, required: true },
+        _userId: { type: Schema.Types.ObjectId, required: true, ref: User },
     },
     { timestamps: true, versionKey: false },
 );
